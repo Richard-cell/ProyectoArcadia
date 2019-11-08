50d9a02fa9c019382ec80ab3203f686415b5af19
Richard-cell <richard3.sanguino@gmail.com>
2019-11-07 20:27:40 -0500

    Agrego WebApi Y Contexto

---
+++ Infraestructure/Contextos/ColegioContext.cs
@@ -0,0 +1,16 @@
+﻿using Infraestructure.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure.Contextos
+{
+    public class ColegioContext: DbContextBase
+    {
+        public ColegioContext(DbContextOptions options):base(options) { 
+
+        }
+        //Colocar dbset Correspondientes
+    }
+}
